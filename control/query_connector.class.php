0e6be5916f8ddc3dcd5ce2f52d1ce25e19e95f93
Joshua Hiwat <mail@joshualonden.com>
2016-09-20 14:35:17 +0200

    Update query_connector.class.php

--- control/query_connector.class.php
+++ control/query_connector.class.php
@@ -1,6 +1,6 @@
 <?php
 
-	class QueryConnector {
+class QueryConnector {
 
     private $connection;
 
@@ -9,100 +9,100 @@
             $this->connection = $connection;
         }
 
-        protected function QueryType($type)	{
-
-			switch ($type) {
-				case 'create':
-					$query_type = 'INSERT INTO';
-					break;
-				case 'read':
-					$query_type = 'SELECT';
-					break;
-				case 'update':
-					$query_type = 'UPDATE';
-					break;
-				case 'delete':
-					$query_type = 'DELETE FROM';
-					break;
-			
-			}
-			return $query_type;
+        protected function QueryType($type)	
+        {
+		switch ($type) {
+			case 'create':
+				$query_type = 'INSERT INTO';
+				break;
+			case 'read':
+				$query_type = 'SELECT';
+				break;
+			case 'update':
+				$query_type = 'UPDATE';
+				break;
+			case 'delete':
+				$query_type = 'DELETE FROM';
+				break;
+		
 		}
-
-		public function processing($type, $table, $column = null) {
-			$query = $this->QueryType($type);
-
-
-			if($type == 'read'):
-
-				$columns = implode(", ",$column);
-
-				$query = ''.$query.' '.$columns.' FROM '.$table.'';
-
-				$dbh_query = $this->connection->getDatabase()->prepare($query);
-
-				$dbh_query->execute();
-
-				$dbh_querys = $dbh_query->fetchAll();
-
-
-				return $dbh_querys;
-
-
-			elseif($type == 'create'):
-
-				$columns = array_keys($column);
-				$col_value = implode(", :",$columns);
-				$col_prepare = implode(", ",$columns);
-
-				$query = ''.$query.' '.$table.' ('.$col_prepare.') VALUES (:'.$col_value.')';
-
-				$dbh_query = $this->connection->getDatabase()->prepare($query);
-
-				$dbh_query->execute($column);	
-
-
-				return $dbh_query;
-
-			elseif($type == 'update'):
-
-				$columns = array_keys($column);
-				$col_set = implode(",",$columns);
-
-				$query_array = [];
-
-				foreach ($column as $key => $value):
-
-					if($key == 'id'):
-						$query_array_id = $key.' = :'.$key;
-					else:
-						$query_array[] = $key.' = :'.$key;
-					endif;
-
-				endforeach;
-
-				$query = ''.$query.' '.$table.' SET '.implode(", ",$query_array).' WHERE '.$query_array_id.'';
-
-				$dbh_query = $this->connection->getDatabase()->prepare($query);
-
-				$dbh_query->execute($column);
-
-
-				return $dbh_query;
-
-			elseif($type == 'delete'):
-
-				$columns = array_keys($column);
-				$col_set = implode(",",$columns);
-
-				$query = ''.$query.' '.$table.' WHERE '.$col_set.'= :'.$col_set.'';
-
-				$dbh_query = $this->connection->getDatabase()->prepare($query);
-
-				$dbh_query->execute($column);
-
-
-				return $dbh_query;
-			endif;
-		}
+		return $query_type;
 	}
+
+	public function processing($type, $table, $column = null) {
+		$query = $this->QueryType($type);
+
+
+		if($type == 'read'):
+
+			$columns = implode(", ",$column);
+
+			$query = ''.$query.' '.$columns.' FROM '.$table.'';
+
+			$dbh_query = $this->connection->getDatabase()->prepare($query);
+
+			$dbh_query->execute();
+
+			$dbh_querys = $dbh_query->fetchAll();
+
+
+			return $dbh_querys;
+
+
+		elseif($type == 'create'):
+
+			$columns = array_keys($column);
+			$col_value = implode(", :",$columns);
+			$col_prepare = implode(", ",$columns);
+
+			$query = ''.$query.' '.$table.' ('.$col_prepare.') VALUES (:'.$col_value.')';
+
+			$dbh_query = $this->connection->getDatabase()->prepare($query);
+
+			$dbh_query->execute($column);	
+
+
+			return $dbh_query;
+
+		elseif($type == 'update'):
+
+			$columns = array_keys($column);
+			$col_set = implode(",",$columns);
+
+			$query_array = [];
+
+			foreach ($column as $key => $value):
+
+				if($key == 'id'):
+					$query_array_id = $key.' = :'.$key;
+				else:
+					$query_array[] = $key.' = :'.$key;
+				endif;
+
+			endforeach;
+
+			$query = ''.$query.' '.$table.' SET '.implode(", ",$query_array).' WHERE '.$query_array_id.'';
+
+			$dbh_query = $this->connection->getDatabase()->prepare($query);
+
+			$dbh_query->execute($column);
+
+
+			return $dbh_query;
+
+		elseif($type == 'delete'):
+
+			$columns = array_keys($column);
+			$col_set = implode(",",$columns);
+
+			$query = ''.$query.' '.$table.' WHERE '.$col_set.'= :'.$col_set.'';
+
+			$dbh_query = $this->connection->getDatabase()->prepare($query);
+
+			$dbh_query->execute($column);
+
+
+			return $dbh_query;
+		endif;
+	}
+}
