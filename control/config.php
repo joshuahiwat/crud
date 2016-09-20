a51dae796824818007ae566e064c0c005e92c681
Joshua Hiwat <mail@joshualonden.com>
2016-09-20 14:35:33 +0200

    Delete config.php

--- control/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-/////////////////////DATABASE CONNECTIE/////////////////////
-$config['database']['host'] 		= "localhost";
-$config['database']['dbname'] 		= "test";
-$config['database']['username'] 	= "root";
-$config['database']['password']		= "";
-?>
