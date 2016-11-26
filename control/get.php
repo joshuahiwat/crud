1e0a49cb26eb8b49377af192f0bece53f78fedd3
Joshua Hiwat <mail@joshualonden.com>
2016-11-26 16:26:13 +0100

    Update get.php

--- control/get.php
+++ control/get.php
@@ -2,7 +2,7 @@
   require_once('query_connector.class.php');
 
   $postFile = new QueryConnector();
-  $test_echo = $postFile->processing('read', 'test', array('column1', 'column2'));
+  $test_echo = $postFile->processing('read', 'test', ['column1', 'column2']);
 
   echo '<pre>';
   var_dump($test_echo);
