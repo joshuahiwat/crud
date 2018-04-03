aa7adac6320458e13a5d839879b4405241759533
Joshua Hiwat <mail@joshualonden.com>
2018-04-03 17:32:39 +0200

    Update database connection to 7.1
    Update database connection. Refactor variables with visibility constant.

--- control/Connection.class.php
+++ control/Connection.class.php
@@ -2,16 +2,16 @@
 
 class Connection
 {
+    private const HOST = "LOCALHOST";
+    private const DATABASE = "...";
+    private const USERNAME = "...";
+    private const PASSWORD = "...";
+    
     public function getDatabase() {
         $dbh = NULL;
 
-        $host = "localhost";
-        $dbname = "test";
-        $username = "root";
-        $password = "";
-
         try {
-            $dbh = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $username, $password);
+            $dbh = new PDO('mysql:host=' . SELF::HOST . ';dbname=' . SELF::DATABASE, SELF::USERNAME, SELF::PASSWORD);
             $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $e) {
             die('DATABASE ERROR: ' . $e->getMessage());
